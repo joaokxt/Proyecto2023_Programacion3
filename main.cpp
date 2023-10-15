41c87ed260af2038d0d686b0b1971f6f04aaac42
Rous444 <118020966+Rous444@users.noreply.github.com>
2023-10-15 17:07:28 -0300

    asd

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <ctime>
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
 
 using namespace std;
 
@@ -9,13 +12,59 @@ int main() {
     cout << "Comenzando a medir Tiempo\n" << endl;
 
     begin = clock();
-    /*
-     * Insertar código a medir tiempo aquí
-     */
+    
+    std::ifstream archivoCSV("Inventariado Fisico.csv");
+
+    const int buffer_size = 4096;  // Tamaño del búfer (ajusta según tus necesidades)
+    char buffer[buffer_size];
+
+    archivoCSV.getline(buffer, buffer_size);  // Lee una línea del archivo en el búfer
+
+    std::stringstream ss(buffer);
+    std::string columna;
+    int contadorColumnas = 0;
+
+    while (std::getline(ss, columna, ',')) {
+        contadorColumnas++;
+    }
+
+    std::cout << "El archivo CSV tiene " << contadorColumnas << " columnas." << std::endl;
+
+    char seprador = ',';
+    string linea;
+    getline(archivoCSV,linea);
+
+    cout<<linea<<endl;
+    
+    contadorColumnas -= 3;
+    string* d = new string[contadorColumnas];
+
+    while(getline(archivoCSV,linea)){
+        stringstream stream(linea);
+        string grupo,codigo,articulo;
+
+        getline(stream,grupo,seprador);
+        getline(stream,codigo,seprador);
+        getline(stream,articulo,seprador);
+        cout<<"================"<<endl;
+        cout<<"Grupo: "<<grupo<<endl;
+        cout<<"Codigo: "<<codigo<<endl;
+        cout<<"Articulo: "<<articulo<<endl;
+        
+        for(int i=0; i<contadorColumnas; i++){
+            getline(stream, d[i], seprador);
+            cout<<"Deposito "<<i+1<<": "<<d[i]<<endl;
+        }
+
+    } 
+    archivoCSV.close();
+    
     clock_t end = clock();
 
     double elapsed_secs = static_cast<double>(end - begin) / CLOCKS_PER_SEC;
 
     cout << "Tardo elapsed_secs" << elapsed_secs << "\n" << std::endl;
-    return 0;
 }
+
+
+  
